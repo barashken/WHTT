145fac19b3e5b0bf846ed52a12a0b261a2b79247
barashken <hapoelbar@gmail.com>
2023-08-21 20:42:44 +0300

    adding database to server

--- server/users/views.py
+++ server/users/views.py
@@ -1,8 +1,7 @@
 from django.views import View
 from django.http import JsonResponse
-from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import redirect
+from pymongo import MongoClient
 from .models import MyUser
 import bcrypt
 import json
@@ -21,11 +20,26 @@ class RegisterView(View):
 
                 hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-                if MyUser.objects.filter(email=email).exists():
+                client = MongoClient('mongodb://localhost:27017/')
+                db = client['wutt']
+                users_collection = db['users']
+
+                if users_collection.find_one({'email': email}):  # If email already exists
                     return JsonResponse({'error': 'Email already exists'}, status=400)
 
-                user = MyUser(first_name=first_name, last_name=last_name, email=email, password=hashed_password)
-                user.save()
+                # if MyUser.objects.filter(email=email).exists():
+                #     return JsonResponse({'error': 'Email already exists'}, status=400)
+
+                user_data = {
+                    'first_name': first_name,
+                    'last_name': last_name,
+                    'email': email,
+                    'password': hashed_password.decode('utf-8'),  # Store as a string
+                }
+                users_collection.insert_one(user_data)
+
+                # user = MyUser(first_name=first_name, last_name=last_name, email=email, password=hashed_password)
+                # user.save()
 
                 # return redirect(reverse('login'))
 
--- server/wuttserver/settings.py
+++ server/wuttserver/settings.py
@@ -77,14 +77,16 @@ WSGI_APPLICATION = 'wuttserver.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'djongo',
-    #     'NAME': 'WUTT-DB',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'wutt',
+        'HOST': 'localhost',
+        'PORT': 27017,  # MongoDB default port
+    }
 }
 
 
